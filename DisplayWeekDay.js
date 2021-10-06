c3ebed77f4f6e650505dc31d03e48c3d06aef183
govind6498 <govindsingh6498@gmail.com>
2021-10-06 07:22:53 +0530

    Added:Print number in words

---
+++ DisplayWeekDay.js
@@ -0,0 +1,28 @@
+let number = 5;
+if(number == 0){
+    console.log("Sunday");
+}
+else if(number == 1){
+    console.log("Monday");
+}
+else if(number == 2){
+    console.log("Tuesday");
+}
+else if(number == 3){
+    console.log("Wednesday");
+}
+else if(number == 4){
+    console.log("Thursday");
+}
+else if(number == 5){
+    console.log("Friday");
+}
+else if(number == 6){
+    console.log("Saturday");
+}
+else if(number == 7){
+    console.log("Sunday");
+}
+else{
+    console.log("Please Enter valid input")
+}
