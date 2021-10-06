be2e9fe06511569718da2542e2dcab6ad5550acf
govind6498 <govindsingh6498@gmail.com>
2021-10-06 07:21:13 +0530

    Added:Print number in word

---
+++ DisplayUnitTenHun.js
@@ -0,0 +1,22 @@
+let number = 100;
+if(number == 1){
+    console.log("Unit");
+}
+else if(number == 10){
+    console.log("Ten");
+}
+else if(number == 100){
+    console.log("Hundred");
+}
+else if(number == 1000){
+    console.log("Thousand");
+}
+else if(number == 10000){
+    console.log("Ten Thousand");
+}
+else if(number == 100000){
+    console.log("Lac")
+}
+else{
+    console.log("Please Enter valid Input")
+}
