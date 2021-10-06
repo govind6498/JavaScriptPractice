e020aa801701d0e8d095c9ebd6b307146360217a
govind6498 <govindsingh6498@gmail.com>
2021-10-06 07:35:35 +0530

    Added:find nth harmonic number

---
+++ Harmonic.js
@@ -0,0 +1,11 @@
+let number = 12;
+let NthValue = 0;
+if(number>0){
+        for(let i=1;i<=number;i++){
+            NthValue = i;
+        }
+        console.log("Nth Harmonic value is:"+"1/"+NthValue)
+        }
+else{
+    console.log("Please Enter valid Input:")
+}
